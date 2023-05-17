e8fb01324655af5c18aad51bfec9bb9bec340ab5
nmer <2309382194@qq.com>
2023-05-17 16:21:32 +0800

    make grid

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -4,7 +4,14 @@ import './home.scss'
 const home=()=>{
     return(
         <div className="home">
-            <div className="header"><img src={require("../images/header-new.png")} alt={"none"}/></div>
+            <header className="header"><img src={require("../images/header-new.png")} alt={"none"}/></header>
+            <main>
+                <section className="section1"></section>
+                <section className="section2"></section>
+                <section className="section3"></section>
+                <section className="section4"></section>
+                <section className="section5"></section>
+            </main>
         </div>
     )
 }
